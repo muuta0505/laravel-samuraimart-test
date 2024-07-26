7f5b3efb87e6619e7d097b4e6c64d961bee5b94e
muuta0505 <kouhei9582@icloud.com>
2024-07-26 12:53:43 +0900

    4回目のコミット

--- laravel-posting-app/app/Http/Controllers/PostController.php
+++ laravel-posting-app/app/Http/Controllers/PostController.php
@@ -72,6 +72,11 @@ class PostController extends Controller
          if ($post->user_id !== Auth::id()) {
              return redirect()->route('posts.index')->with('error_message', '不正なアクセスです。');
          }
+
+         $request->validate([
+            'title' => 'required|max:40',
+            'content' => 'required|max:200'
+        ]);
  
          $post->title = $request->input('title');
          $post->content = $request->input('content');
